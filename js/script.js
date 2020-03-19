ca159f845b31e4b656b06a930d30e6c7522e1a07
Carlos Corti <carlosacorti@hotmail.com>
2020-03-19 12:56:50 +0100

    web async with .ajax
    ajax requests to unpsplash, wikipeia and new yor times

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 
-function loadData() {
+function loadData(event) {
+
+    event.preventDefault();
+
+    $('.bgimg').remove();
 
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
@@ -11,60 +15,96 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ', ' + cityStr;
+    const street = $('#street').val();
+    const city = $('#city').val();
+    const wikyCity = city.replace(" ", "%20");
 
-    $greeting.text('So, you want to live at ' + address + '?');
+    console.log(street, city, wikyCity);
+
+    $.ajax ({
+        url: `https://api.unsplash.com/search/photos/?query=${city}`,
+        headers: 
+        {
+            Authorization: 'Client-ID TYt3-4mzjJgzSYf2j7BV8SM3ScyHbulpkuBFhLaGxuY'
+        }, 
+        dataType: "json"
+    }).done(unsplashHandler)
+    .fail(e => errorHandler (e, "pic", $greeting));
+
+    $.ajax ({
+        url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${city}&api-key=zM2a71pD2xeBLFePq6KcpuhGkfpY9eXG`,
+         
+        dataType: "json"
+    }).done(nytHandler)
+    .fail(e => errorHandler (e, "article", $nytElem));
+
+    $.ajax ({
+        url: `https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=${wikyCity}&prop=info&inprop=url&format=json&origin=*`,
+        
+        dataType: "json",
+
+    }).done(wikiHandler)
+    .fail(e => errorHandler (e, "wiki charge",  $wikiElem));
+    
+    function errorHandler (errorThrown, itemString, domElement) {
+        console.log(`There was an error loading the ${itemString}, error: ${errorThrown}`);
+        domElement.text(`No ${itemString} found`);
+    }
+    
+
+    function unsplashHandler (response) {
+        if (response && response.results && response.results.length){
+            const img = Math.round(Math.random()*(response.results.length-1));
+            $body.append(`<img class="bgimg" src="${response.results[img].urls.regular}" alt="Selected adress and city">`)
+        }else{
+            console.log("unpls error working")
+            return Error("No pic found")
+        }
+    }
+
+    function nytHandler (data) {
+        if (data && data.response.docs && data.response.docs.length && city){
+            console.log(data, data.response.docs, data.response.docs.length);
+            data.response.docs.map (article =>
+                    $nytElem.append(`<li class="article">
+                                        <a href='${article.web_url}' target="_blank">${article.headline.main}</a>
+                                        <p>${article.abstract}</p>                            
+                                    </li>`)
+            );
+        }else{
+            console.log("nyt error working")
+            return Error("No article found")
+        }
+    }
+
+    function wikiHandler (data){
+        if (data && data.query && Object.values(data.query.pages).length){
+            console.log(data, data.query.pages, Object.values(data.query.pages).length);
+            for (element of Object.values(data.query.pages)){
+                //console.log(element.fullurl)
+                $wikiElem.append(`<li>
+                                    <a href='${element.fullurl}' target="_blank">${element.title}</a>
+                                </li>`)
+            };
+        }else{
+            console.log("wiki error working")
+            return Error("No wiki found")
+        };
+        //Object.values(data.query.pages).forEach(element => console.log(element.fullurl)
+    };
+
+
+        /*
+        const img = Math.round(Math.random()*(response.results.length-1));
+        $body.append(`<img class="bgimg" src="${response.results[img].urls.regular}" alt="Selected adress and city">`)
+        */
+    
+
+
 
     // load streetview
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
-
-    // load nytimes
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=3abc9a3d23e60b38c21b4ab9b0a91c07:17:69911633'
-    $.getJSON(nytimesUrl, function(data){
-
-        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
-
-        articles = data.response.docs;
-        for (var i = 0; i < articles.length; i++) {
-            var article = articles[i];
-            $nytElem.append('<li class="article">'+
-                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-                '<p>' + article.snippet + '</p>'+
-            '</li>');
-        };
-
-    }).error(function(e){
-        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
-    });
-
-
-
-    // load wikipedia data
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
-    var wikiRequestTimeout = setTimeout(function(){
-        $wikiElem.text("failed to get wikipedia resources");
-    }, 8000);
-
-    $.ajax({
-        url: wikiUrl,
-        dataType: "jsonp",
-        jsonp: "callback",
-        success: function( response ) {
-            var articleList = response[1];
-
-            for (var i = 0; i < articleList.length; i++) {
-                articleStr = articleList[i];
-                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-            };
-
-            clearTimeout(wikiRequestTimeout);
-        }
-    });
+    // YOUR CODE GOES HERE!
 
     return false;
 };
