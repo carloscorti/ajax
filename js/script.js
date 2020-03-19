d44451d710a919d6d1135760d9ccdd70e0fb560c
Carlos Corti <carlosacorti@hotmail.com>
2020-03-19 14:00:23 +0100

    fetch ajax request solution
    fetch ajax request to unsplash new york times and wikipedia

--- js/script.js
+++ js/script.js
@@ -19,32 +19,24 @@ function loadData(event) {
     const city = $('#city').val();
     const wikyCity = city.replace(" ", "%20");
 
-    console.log(street, city, wikyCity);
+    function requestJson (url, header={}){
+        return fetch(url, {
+            headers: header
+        }).then(response => response.json());
+    }
 
-    $.ajax ({
-        url: `https://api.unsplash.com/search/photos/?query=${city}`,
-        headers: 
-        {
-            Authorization: 'Client-ID TYt3-4mzjJgzSYf2j7BV8SM3ScyHbulpkuBFhLaGxuY'
-        }, 
-        dataType: "json"
-    }).done(unsplashHandler)
-    .fail(e => errorHandler (e, "pic", $greeting));
+    requestJson(`https://api.unsplash.com/search/photos/?query=${city}`,
+    {'Authorization': 'Client-ID TYt3-4mzjJgzSYf2j7BV8SM3ScyHbulpkuBFhLaGxuY'})
+    .then(unsplashHandler)
+    .catch(e => errorHandler (e, "pic", $greeting))
 
-    $.ajax ({
-        url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${city}&api-key=zM2a71pD2xeBLFePq6KcpuhGkfpY9eXG`,
-         
-        dataType: "json"
-    }).done(nytHandler)
-    .fail(e => errorHandler (e, "article", $nytElem));
+    requestJson(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${city}&api-key=zM2a71pD2xeBLFePq6KcpuhGkfpY9eXG`)
+    .then(nytHandler)
+    .catch(e => errorHandler (e, "article", $nytElem))
 
-    $.ajax ({
-        url: `https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=${wikyCity}&prop=info&inprop=url&format=json&origin=*`,
-        
-        dataType: "json",
-
-    }).done(wikiHandler)
-    .fail(e => errorHandler (e, "wiki charge",  $wikiElem));
+    requestJson(`https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=${wikyCity}&prop=info&inprop=url&format=json&origin=*`)
+    .then(wikiHandler)
+    .catch(e => errorHandler (e, "wiki",  $wikiElem));
     
     function errorHandler (errorThrown, itemString, domElement) {
         console.log(`There was an error loading the ${itemString}, error: ${errorThrown}`);
@@ -58,13 +50,12 @@ function loadData(event) {
             $body.append(`<img class="bgimg" src="${response.results[img].urls.regular}" alt="Selected adress and city">`)
         }else{
             console.log("unpls error working")
-            return Error("No pic found")
+            throw Error("No pic found")
         }
     }
 
     function nytHandler (data) {
         if (data && data.response.docs && data.response.docs.length && city){
-            console.log(data, data.response.docs, data.response.docs.length);
             data.response.docs.map (article =>
                     $nytElem.append(`<li class="article">
                                         <a href='${article.web_url}' target="_blank">${article.headline.main}</a>
@@ -73,13 +64,12 @@ function loadData(event) {
             );
         }else{
             console.log("nyt error working")
-            return Error("No article found")
+            throw Error("No article found")
         }
     }
 
     function wikiHandler (data){
         if (data && data.query && Object.values(data.query.pages).length){
-            console.log(data, data.query.pages, Object.values(data.query.pages).length);
             for (element of Object.values(data.query.pages)){
                 //console.log(element.fullurl)
                 $wikiElem.append(`<li>
@@ -88,27 +78,12 @@ function loadData(event) {
             };
         }else{
             console.log("wiki error working")
-            return Error("No wiki found")
+            throw Error("No wiki found")
         };
-        //Object.values(data.query.pages).forEach(element => console.log(element.fullurl)
     };
 
-
-        /*
-        const img = Math.round(Math.random()*(response.results.length-1));
-        $body.append(`<img class="bgimg" src="${response.results[img].urls.regular}" alt="Selected adress and city">`)
-        */
-    
-
-
-
-    // load streetview
-
-    // YOUR CODE GOES HERE!
 
     return false;
 };
 
 $('#form-container').submit(loadData);
-
-// loadData();
